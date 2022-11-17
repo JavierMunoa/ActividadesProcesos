640e61718a51cfe4c748418b6d8ef30b8ed69ad4
JavierMunoa <93648728+JavierMunoa@users.noreply.github.com>
2022-11-17 11:12:13 +0100

    Correccion Hilos durmientes

--- HilosDurmientesC2C/src/HiloDurmiente.java
+++ HilosDurmientesC2C/src/HiloDurmiente.java
@@ -5,7 +5,7 @@ public class HiloDurmiente extends Thread{
         while (true){
             try {
                 System.out.println("Soy el bucle :" + getName() +" y estoy trabajando");
-                Thread.sleep(1000,100000);
+                Thread.sleep(1000*(int)(Math.random()*10+1));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
