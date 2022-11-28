c7146fba14ed78bdfea29f4ef3eb2e8badb1c368
JavierMunoa <93648728+JavierMunoa@users.noreply.github.com>
2022-11-28 11:52:56 +0100

    Actividades libros

---
+++ LibrosEstudiantes/src/Libros.java
@@ -0,0 +1,11 @@
+public class Libros {
+    private boolean tomado;
+
+    public boolean isTomado() {
+        return tomado;
+    }
+
+    public void setTomado(boolean tomado) {
+        this.tomado = tomado;
+    }
+}
