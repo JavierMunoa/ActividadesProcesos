f95afb0729bf0ccdf28a7c09d136c10d6bc77966
JavierMunoa <93648728+JavierMunoa@users.noreply.github.com>
2022-12-06 18:02:20 +0100

    Documentación filósofos

--- Filosofos/src/Filosofo.java
+++ Filosofos/src/Filosofo.java
@@ -5,10 +5,11 @@ public class Filosofo extends Thread{
     public int idFilosofo;
 
     public static void main(String[] args) {
+        //Instanciamos los objetos del arrylist palillos para poder utilizarlos
         for (int i = 0; i < 5; i++) {
             palillos.add(new clsPalillo());
         }
-
+        //Creamos cinco hilos filósofo, les seteamos un nombre, un id y los lanzamos
         Filosofo filosofo = null;
         for (int i = 0; i < 5; i++) {
             filosofo = new Filosofo();
@@ -22,8 +23,12 @@ public class Filosofo extends Thread{
     @Override
     public void run() {
         super.run();
+        /*Instanciaremos dos int's palillos, los cuales se referirán a la posición del arraylist palillo a la que apuntará
+        * cada filósofo para usar los palillos, simbolizando estos los palillos que tendrá cada filósofo a la izquierda y
+        * a la derecha respectivamente.*/
         int palillo1= idFilosofo;
         int palillo2 = idFilosofo+1;
+        //En caso del último filósofo este tendrá a su derecha el primer palillo.
         if (idFilosofo == 4){
             palillo2 = 0;
         }
@@ -34,6 +39,8 @@ public class Filosofo extends Thread{
     public void comer(int palillo1 ,int palillo2) {
         synchronized (palillos){
             try {
+                /*En caso de que el palillo que quiere el filósofo esté en uso, este esperará a que sea liberado, y no
+                tomará el otro palillo que le corresponde*/
                 if (palillos.get(palillo1).isEnUso()){
                     palillos.wait();
                     System.out.println("El filósofo " + this.getName() + " está esperando el palillo " + palillo1);
@@ -45,6 +52,9 @@ public class Filosofo extends Thread{
                 System.out.println("Ha ocurrido un error de interrupción.");
             }
         }
+        /*Si el filósofo ha podido tomar ambos palillos, estos pasarán al estado -enUso cambiando el valor del booleano,
+        * el filósofo comerá y tras esto liberará ambos palillos, informando siempre al usuario del estado tanto de los
+        * palillos como de los filósofos.*/
         palillos.get(palillo1).setEnUso(true);
         palillos.get(palillo2).setEnUso(true);
         System.out.println("El palillo " + palillo1 + " ha sido tomado.");
